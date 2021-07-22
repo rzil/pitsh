1ae846ebb9009514ccca68b052ccf2ea9a9ffd53
Ruben Zilibowitz <ruben@rubenz.net>
2021-07-23 07:16:27 +1000

    Initial Commit

---
+++ Shared/pitshApp.swift
@@ -0,0 +1,17 @@
+//
+//  pitshApp.swift
+//  Shared
+//
+//  Created by Ruben Zilibowitz on 23/7/21.
+//
+
+import SwiftUI
+
+@main
+struct pitshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
